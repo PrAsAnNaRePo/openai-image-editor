af670ea9fc327a31b2c1873ea7d721d54824782d
PrAsAnNaRePo <prasannatwenty@gmail.com>
2025-05-11 18:17:10 +0530

    allow hosts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    strictPort: true,
+    allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      'ewwosck8cg40scokcsscg4o4.4.240.99.23.sslip.io',
+      'all'
+    ]
+  }
 })
